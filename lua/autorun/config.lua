03d309d4e84e6a447fa38eee397bcd0a2042e1c6
Zihad Joy <smz.joy73@gmail.com>
2026-02-22 03:38:13 +0600

    feat: add c and c++ runners with compiler detection

--- lua/autorun/config.lua
+++ lua/autorun/config.lua
@@ -5,10 +5,12 @@ M.options = {}
 local defaults = {
 	term_style = "vertical",
 	term_size = "40",
+	prefered_c_compiler = "gcc",
+	prefered_cpp_compiler = "g++",
 }
 
-function M.setup(opts) 
-    M.options = vim.tbl_deep_extend("force", defaults, opts or {})
+function M.setup(opts)
+	M.options = vim.tbl_deep_extend("force", defaults, opts or {})
 end
 
 return M
